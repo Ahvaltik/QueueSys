5959ed89275a3585812289c2d0cf920f6af00878
Ahvaltik <pawele@vp.pl>
2013-12-03 11:01:35 +0100

    basic test and cleaning

--- QueueSys/src/queuesys/Main.java
+++ QueueSys/src/queuesys/Main.java
@@ -14,6 +14,23 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Cockroaches cockroaches = new Cockroaches();
+        double lambda = 19;
+        double mu = 1;
+        int N = 15;
+        int m = 7;
+        double c1 = 4;
+        double c2 = 12;
+        ICostFunction queueCostFunction = new QueueCostFunction(m, N, lambda, mu, c1, c2);
+        for(int i = 1; i <= N; i ++){
+            System.out.println("value " + i + " = " + queueCostFunction.cost(i));
+        }
+        cockroaches.setCostFunction(queueCostFunction);
+        cockroaches.setDisperseStepSize(3.0);
+        cockroaches.setSwarmStepSize(5.0);
+        cockroaches.setN(15);
+
+        int solution = cockroaches.solve(1000, 1000);
+        System.out.printf("solution is: %d\n", solution);
     }
 }
---
+++ readme.txt
@@ -0,0 +1,2 @@
+NetBeans IDE 7.2.1 Java Project
+QueueSys
